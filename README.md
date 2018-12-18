2235c65ac4f8e37cf7223b9da52025b4ce3d8f0b
Xie Yuheng <xyheme@gmail.com>
2018-12-18 15:54:49 +0800

    再犯

---
+++ README.md
@@ -0,0 +1,10 @@
+# cicada notebook
+
+interactive UI of [cicada-language](https://github.com/xieyuheng/cicada)
+
+- in naked js -- simple and effective
+- wasm in browser -- without server
+- install deps `npm install`
+- offline useage `npx webpack-dev-server`
+
+[Online Demo ^-^/](https://cicada-notebook.surge.sh)
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// const path = require ("path");
+// const webpack = require ("webpack");
+const HtmlWebpackPlugin = require ("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.js",
+    mode: "development",
+    resolve: {
+        extensions: [ ".js", ".json", ".wasm" ],
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: "./src/index.html",
+        }),
+    ],
+};
